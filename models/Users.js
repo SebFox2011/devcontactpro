93595bdd7cff9b1e7ab8b4d4a72cb0e20f30ed30
Seb <sebastien.philippe5@gmail.com>
2019-09-20 11:54:34 +0200

    Ajout des champs phone,email,linkedin,avatat et info + ajout d'un prepersist pour enregistrer la date de création automatiquement à la validation du formulaire

--- models/Users.js
+++ models/Users.js
@@ -12,14 +12,19 @@ const userSchema = new mongoose.Schema ({
     phone: String,
     linkedin: String,
     avatar: String,
-    info: String
+    info: String,
+    createdAt: Date
 });
 
-userSchema.virtual('fullname').get(function () { this.firstname + ' ' + this.lastname});
+userSchema.virtual('fullname').get(function () { return this.firstname + ' ' + this.lastname});
+
+userSchema.pre('save',function(){
+    this.createdAt = new Date (); // pour créer la date de création du compte automatiquement avec le formulaire
+});
 
 userSchema.post('save',user => { // userSchema.pre existe aussi
     //console.log('Nouvel utilisateur: ' + user.firstname +' ' + user.lastname );
     console.log('Nouvel utilisateur: ' + user.fullname );
 });// apres l'enregisytrement d'un utilisateur, execute la fonction , Next permet de passer au middleware suivant
 
-module.exports = mongoose.model('User',userSchema); //USer est le nom de l'entité enregisté
+module.exports = mongoose.model('Users',userSchema); //USer est le nom de l'entité enregisté
